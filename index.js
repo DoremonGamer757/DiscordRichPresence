c00139c2a6c5afe201e4850a0df645ad66377213
I'm Doremon <105568921+DoremonGamer757@users.noreply.github.com>
2024-04-27 19:17:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('Your Application ID')
+    .setApplicationId('1182941320457748611')
     .setType('STREAMING')
-    .setURL('Your Twitch URL') //Must be a youtube video link 
-    .setState('Your State')
-    .setName('mrnekrozyt')
-    .setDetails(`THE NAME IT SHOWS YOUR STREAMING [${formatTime()}]`)
+    .setURL('https://twitch.tv/imdoremon') //Must be a youtube video link 
+    .setState("I'm Doremon")
+    .setName('Doremon On Top')
+    .setDetails(`Playing Doremon Network [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('Large Image URL') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Large Text') //Text when you hover the Large image
-    .setAssetsSmallImage('Small Image URL') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Small Text') //Text when you hover the Small image
-    .addButton('Button 1', 'Button URL')
-    .addButton('Button 2', 'Button URL');
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1175478503957872730/1233772829657399357/Doremon_Network_Animated_Banner_2.gif?ex=662e4ffc&is=662cfe7c&hm=5cfdda4798264594c2b2c74ec4512b6c36cb419ea2335356bb82df1124892a72&') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Doremon Network') //Text when you hover the Large image
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1175478503957872730/1233775521469562890/Im_Doremon_Glitch.gif?ex=662e527e&is=662d00fe&hm=6ab9452b02a6a26c7d1e5efc41f993f7fb8479f2dcab951fa3471f9751f98211&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText("I'm Doremon") //Text when you hover the Small image
+    .addButton('Youtube Channel', 'https://www.youtube.com/channel/UCvQ4dmqsuzzgyxP93tFvVSA')
+    .addButton('Discord Server', 'https://www.dsc.gg/imdoremon');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
